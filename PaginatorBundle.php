2f29b62af1e46d781b47b75d72040d72ffb5d705
dimmir <petelindv@gmail.com>
2017-05-19 22:50:17 +0700

    enable comliler pass for factory

--- PaginatorBundle.php
+++ PaginatorBundle.php
@@ -2,8 +2,16 @@
 
 namespace DMR\Bundle\PaginatorBundle;
 
+use DMR\Bundle\PaginatorBundle\DependencyInjection\Compiler\PaginatorFactoryPass;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class PaginatorBundle extends Bundle
 {
+    public function build(ContainerBuilder $container)
+    {
+        parent::build($container);
+
+        $container->addCompilerPass(new PaginatorFactoryPass());
+    }
 }
